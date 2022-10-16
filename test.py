6cb060b2dbf1b7fe3784df6916bd12d70ec550cf
Nana2929 <58811089+Nana2929@users.noreply.github.com>
2022-10-16 13:36:00 +0800

    add: testing

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 """
 =================================
 
-TESTING
+TESTING SCRIPT
 
 =================================
 """
@@ -10,45 +10,76 @@ from mlxtend.frequent_patterns import fpgrowth, association_rules
 from mlxtend.preprocessing import TransactionEncoder
 import pandas as pd
 from utils import read_file, timer
-# my fpgrowth algorithm
+# ==========My Own Packages============
+import fptree
+import importlib
+importlib.reload(fptree)
 from fptree import *
+from apriori import *
 
 
-# 2022 testdata
+MINSUP, MINCONF = 0.3, 0.3
 filepath = '/Users/yangqingwen/Downloads/ibm-2022-release-testdata-1/2022-DM-release-testdata-2.data'
+# filepath = '/Users/yangqingwen/Downloads/IBM2021.txt'
+
 data = read_file(
     filepath)
 transactions = preprocess(data)
-MINSUP, MINCONF = 0.1, 0.1
+#%%
+# =========== My Apriori ===============
 @timer
-def getMyRules():
+def myApriori():
+    global myapriori
+    myapriori= Apriori(transactions, minsup = MINSUP, minconf = MINCONF)
+    apPatterns = myapriori.gen_freq_patterns()
+    print(f'My Freqsets length: {len(apPatterns)}')
+    apRules = myapriori.get_rules(apPatterns)
+    print(f'My Rules length: {len(apRules)}')
+    return apRules, apPatterns
+apRules, apPatterns = myApriori()
+apPatterns = set(apPatterns)
+
+#%%
+# =========== My FPGrowth ===============
+@timer
+def myFP():
     global mainTree
     mainTree = makeTree(transactions, min_support_ratio = MINSUP)
     freqPatternDict = mineTree(mainTree)
-    freqPatterns = flatten(freqPatternDict)
-    MyRules = getAssociationRules(transactions, freqPatterns, minconf = MINCONF)
-    return MyRules, freqPatterns
-MyRules, MySets = getMyRules()
+    fpPatterns = flatten(freqPatternDict)
+    fpRules = getAssociationRules(transactions, fpPatterns, minconf = MINCONF)
+    print(f'My Freqsets length: {len(fpPatterns)}')
+    print(f'My Rules length: {len(fpRules)}')
+    return fpRules, fpPatterns
+fpRules, fpPatterns = myFP()
 
+#%%
+# =========== Mlxtend Apriori ===============
+from mlxtend.frequent_patterns import fpgrowth, apriori, association_rules
 @timer
-def runLibFP(transactions):
+def LibApriori():
+    te = TransactionEncoder()
+    te_ary = te.fit(transactions).transform(transactions)
+    df = pd.DataFrame(te_ary, columns=te.columns_)
+    freqsets = apriori(df, min_support=MINSUP)
+    LibRules = association_rules(freqsets, metric="confidence", min_threshold=MINCONF)
+    print(f'Lib FreqSets Size: {len(freqsets)}')
+    print(f'Lib Rules length: {len(LibRules)}')
+LibApriori()
+# %%
+# =========== Mlxtend FpGrowth ===============
+@timer
+def LibFP():
     te = TransactionEncoder()
     te_ary = te.fit(transactions).transform(transactions)
     df = pd.DataFrame(te_ary, columns=te.columns_)
     # global freqsets
     freqsets = fpgrowth(df, min_support=MINSUP, use_colnames=True)
-    rules = association_rules(freqsets, metric="confidence", min_threshold=MINCONF)
-    return rules, freqsets
-LibRules, LibSets = runLibFP(transactions)
-
-print(f'MINCONF: {MINCONF}, MINSUP: {MINSUP}')
-print('============MINE===========')
-print('rules:', len(MyRules))
-print('sets:', len(MySets))
-print('===========MLXTEND============')
-print('rules:', len(LibRules))
-print('sets:', len(LibSets))
+    librules = association_rules(freqsets, metric="confidence", min_threshold=MINCONF)
+    print(f'Lib FreqSets Size: {len(freqsets)}')
+    print(f'Lib Rules length: {len(librules)}')
+LibFP()
 
 
-# %%
+
 
