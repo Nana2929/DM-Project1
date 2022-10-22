6fa395b006604ba9aac9efd6b7b9ac5714a99b69
Nana2929 <58811089+Nana2929@users.noreply.github.com>
2022-10-23 00:17:07 +0800

    mod: tree

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 """
 =================================
 
-TESTING
+TESTING SCRIPT
 
 =================================
 """
@@ -9,45 +9,120 @@ TESTING
 from mlxtend.frequent_patterns import fpgrowth, association_rules
 from mlxtend.preprocessing import TransactionEncoder
 import pandas as pd
-from utils import read_file, timer
-# my fpgrowth algorithm
+from sympy import re
+from utils import read_file, timer, preprocess
+# ==========My Own Packages============
+import fptree
+# import importlib
+# importlib.reload(fptree)
 from fptree import *
+from apriori import *
 
+filepath = '/Users/yangqingwen/Desktop/Github/DM-Project1/inputs/target.txt'
+# filepath = '/Users/yangqingwen/Downloads/IBM2021.txt'
 
-# 2022 testdata
-filepath = '/Users/yangqingwen/Downloads/ibm-2022-release-testdata-1/2022-DM-release-testdata-2.data'
 data = read_file(
     filepath)
 transactions = preprocess(data)
-MINSUP, MINCONF = 0.1, 0.1
+
+#%%
+# =========== My Apriori ===============
 @timer
-def getMyRules():
+def myApriori(MINSUP, MINCONF):
+    global myapriori
+    myapriori= Apriori(transactions, minsup = MINSUP, minconf = MINCONF)
+    apPatterns = myapriori.gen_freq_patterns()
+    print(f'My Freqsets length: {len(apPatterns)}')
+    apRules = myapriori.get_rules(apPatterns)
+    print(f'My Rules length: {len(apRules)}')
+    return apRules, apPatterns
+
+# myapriori= Apriori(transactions, minsup = 0.1, minconf = 0.1)
+# apPatterns = set(apPatterns)
+
+#%%
+# =========== My FPGrowth ===============
+@timer
+def myFP(MINSUP, MINCONF):
     global mainTree
     mainTree = makeTree(transactions, min_support_ratio = MINSUP)
     freqPatternDict = mineTree(mainTree)
-    freqPatterns = flatten(freqPatternDict)
-    MyRules = getAssociationRules(transactions, freqPatterns, minconf = MINCONF)
-    return MyRules, freqPatterns
-MyRules, MySets = getMyRules()
+    fpPatterns = flatten(freqPatternDict)
+    fpRules = getAssociationRules(transactions, fpPatterns, minconf = MINCONF)
+    print(f'My Freqsets length: {len(fpPatterns)}')
+    print(f'My Rules length: {len(fpRules)}')
+    return fpRules, fpPatterns
 
+
+#%%
+# =========== Mlxtend Apriori ===============
+from mlxtend.frequent_patterns import fpgrowth, apriori, association_rules
 @timer
-def runLibFP(transactions):
+def LibApriori(MINSUP, MINCONF):
     te = TransactionEncoder()
     te_ary = te.fit(transactions).transform(transactions)
     df = pd.DataFrame(te_ary, columns=te.columns_)
-    # global freqsets
-    freqsets = fpgrowth(df, min_support=MINSUP, use_colnames=True)
-    rules = association_rules(freqsets, metric="confidence", min_threshold=MINCONF)
-    return rules, freqsets
-LibRules, LibSets = runLibFP(transactions)
+    freqsets = apriori(df, min_support=MINSUP)
+    LibRules = association_rules(freqsets, metric="confidence", min_threshold=MINCONF)
+    print(f'Lib FreqSets Size: {len(freqsets)}')
+    print(f'Lib Rules length: {len(LibRules)}')
 
-print(f'MINCONF: {MINCONF}, MINSUP: {MINSUP}')
-print('============MINE===========')
-print('rules:', len(MyRules))
-print('sets:', len(MySets))
-print('===========MLXTEND============')
-print('rules:', len(LibRules))
-print('sets:', len(LibSets))
+# %%
+# =========== Mlxtend FpGrowth ===============
+@timer
+def LibFP(MINSUP, MINCONF):
+    te = TransactionEncoder()
+    te_ary = te.fit(transactions).transform(transactions)
+    df = pd.DataFrame(te_ary, columns=te.columns_)
+    freqsets = fpgrowth(df, min_support=MINSUP, use_colnames=True)
+    librules = association_rules(freqsets, metric="confidence", min_threshold=MINCONF)
+    print(f'Lib FreqSets Size: {len(freqsets)}')
+    print(f'Lib Rules length: {len(librules)}')
+    return librules, freqsets
+
+
+#%%
+
+# %%
+minsup = 0.1
+rulesizes = []
+confs = []
+for i in range(9):
+    minconf = 0.1 + i*0.1
+    aprules, appatterns = myApriori(MINSUP = minsup, MINCONF=minconf)
+    rulesizes.append(len(aprules))
+    confs.append(minconf)
+#%%
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_theme(style="darkgrid")
+ax = sns.lineplot(confs,rulesizes)
+ax.set(title='Rule Size Tendency with minsup = 0.1')
+ax.set(xlabel='minconf', ylabel='rule size')
+plt.show()
+#%%
+fixed_conf_rulesizes = []
+fixed_conf_freqp = []
+sups = []
+minconf = 0.1
+for i in range(9):
+    minsup = 0.1 + i*0.1
+    aprules, appatterns = myApriori(MINSUP = minsup, MINCONF=minconf)
+    fixed_conf_rulesizes.append(len(aprules))
+    fixed_conf_freqp.append(len(appatterns))
+    sups.append(minsup)
+#%%
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_theme(style="darkgrid")
+ax = sns.lineplot(sups,fixed_conf_rulesizes)
+ax.set(title='Rule Size Tendency with minconf = 0.1')
+ax.set(xlabel='minsup', ylabel='rule size')
+plt.show()
+
+
+
+
 
 
 # %%
--- trie.py
+++ trie.py
@@ -1,4 +1,5 @@
 
+#%%
 from typing import Optional, Dict, List
 
 class TrieNode:
@@ -17,32 +18,30 @@ class Trie:
         self.plen = k - 2
 
     def insert(self, itemset:List[int]) -> None:
-        """Insert itemset of Lk-1 in to the trie,
-        each itemset is in length of k-1 in iteration k  of aprior
+        """Insert sorted itemset of Lk-1 in to the trie,
+        each itemset is in length of k-1 in iteration k of apriori
         Args:
-            itemset (List[int]): itemset in Lk-1
+            itemset (List[int]): itemset in Lk-1; sorted
         """
         assert len(itemset) == self.isize
         node = self.root
-        itemset.sort()
         for item in itemset:
             if item not in node.children:
                 node.children[item] = TrieNode(item)
             node = node.children[item]
 
     def get_children(self, prefix: List[int]) -> Dict:
-        """get children based on itemset prefix
-            prefix is in length of k-2 in iteration k of apriori
+        """get children based on sorted itemset prefix
+            prefix's length is k-2 in iteration k of apriori
         Args:
-            prefix (List[int]): a subset of the itemset (prefix)
+            prefix (List[int]): a subset of the itemset (prefix); sorted
         Returns:
             Dict: children; the children of the prefix;
             itemsets can be transformed back as prefix.union(c) for c in children
         """
         assert len(prefix) == self.plen
         node = self.root
-        prefix.sort()
-        for p in prefix:
+        for p in prefix: # traverse down th prefix path
             node = node.children[p]
         return node
 
@@ -51,14 +50,3 @@ class Trie:
         """
         node = self.get_children(prefix)
         return [c for c in node.children.keys()]
-# t = Trie(k = 4)
-# t.insert([1,2,3])
-# t.insert([1,2,4])
-# t.insert([2,3,5])
-# t.insert([2,4,8])
-# t.insert([2,3,11])
-# c1 = t.get_clean_children(prefix = [1,2])
-# print(c1)
-# c2 = t.get_clean_children(prefix = [2,3])
-# print(c2)
-
